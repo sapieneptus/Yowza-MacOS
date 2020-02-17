88490c48675cb2b57634bd629a0e8cd5f9f73244
Chris Fuentes <cfuentes@g.harvard.edu>
2020-02-17 13:20:01 +0100

    works?

--- Yowza-MacOS/Config.swift
+++ Yowza-MacOS/Config.swift
@@ -3,7 +3,7 @@ import Foundation
 import HockeySDK
 
 class Config: NSObject {
-    static let shared = Config()
+    static let shared = try! Config()
     enum Environment : String {
         case Training = "https://training.hockeyapp.net/"
         case Rink = "https://rink.hockeyapp.net/"
@@ -13,23 +13,27 @@ class Config: NSObject {
     let appId: String
     let feedURL: String
 
-    override init() {
+    enum ConfigError: Error {
+        case MissingValue(_ value: String)
+    }
+
+    init(_: Void?=nil) throws {
         let haEnv = Bundle.main.infoDictionary?["HA_ENV"] as! String
         if haEnv == "training" {
             self.environment = Environment.Training
         } else if  haEnv == "rink" {
             self.environment = Environment.Rink
         } else {
-            abort()
+            throw ConfigError.MissingValue("HA_ENV")
         }
 
         guard let haAppId = Bundle.main.infoDictionary?["HA_APP_ID"] as? String else {
-            abort()
+            throw ConfigError.MissingValue("HA_APP_ID")
         }
         self.appId = haAppId
 
         guard let feedURL = Bundle.main.infoDictionary?["SUFeedURL"] as? String else {
-            abort()
+            throw ConfigError.MissingValue("SUFeedURL")
         }
         self.feedURL = feedURL
     }
